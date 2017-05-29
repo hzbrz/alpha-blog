e281773d07753c7390594a196f030c7f7edcb037
Rails-Blog <hsz201@email.vccs.edu>
2017-05-29 17:05:43 -0500

    Added users listing and update show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  before_action :set_user_instance_to_id, only: [:edit, :show, :update]
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -7,27 +14,33 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end
 
   def edit
-    @user = User.find(params[:id])
+  end
+
+  def show
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was successfully updated!"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
   end
 
   private
+    def set_user_instance_to_id
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  # passing in another that controls the profile pic size...defaults to 80
+  def gravatar_for(user, options = { size: 80 })
+    # Gravatar url's are based on a MD5 hash of the user's email address, and in Ruby this MD5 algorithm is implemented
+    # using the hexdigest method
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    # setting the size var to the value of the size 
+    size = options[:size]
+    gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    # image_tag basically created an image tag with gravatar_url as src
+    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+  end
 end
